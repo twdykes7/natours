869ec12e6a619aa7e28cbfc9a9d1fa06462d8b0d
Timothy Dykes <timothywanedykes@gmail.com>
2021-10-19 22:49:12 -0700

    Seperate and mount user and tour routers

--- app.js
+++ app.js
@@ -143,32 +143,33 @@ const createUser = (req, res) => {
 
 // Routes
 
-// app.get('/api/v1/tours', getAllTours)
-// app.post('/api/v1/tours', createTour)
-// app.get('/api/v1/tours/:id', getTour)
-// app.patch('/api/v1/tours/:id', updateTour)
-// app.delete('/api/v1/tours/:id', deleteTour)
+const tourRouter = express.Router();
+const userRouter = express.Router()
+tourRouter
+.route('/')
+.get(getAllTours)
+.post(createTour)
 
-app
-  .route('/api/v1/tours')
-  .get(getAllTours)
-  .post(createTour)
+tourRouter
+.route('/:id')
+.get(getTour)
+.patch(updateTour)
+.delete(deleteTour)
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour)
 
-app.route('/api/v1/users')
-  .get(getAllUsers)
-  .post(createUser)
+userRouter
+.route('/')
+.get(getAllUsers)
+.post(createUser)
 
-app.route('/api/v1/user/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser)
+userRouter
+.route('/:id')
+.get(getUser)
+.patch(updateUser)
+.delete(deleteUser)
 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter)
 
 // Start Server
 const port = 3000;
