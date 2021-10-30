fa7712c513f2eb23953b478b33faddb02d470d7d
Timothy Dykes <timothywanedykes@gmail.com>
2021-10-29 17:02:22 -0700

    Add pagination feature for querying

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,12 +25,23 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
-    //Field limiting
+    // Field limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 1;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     // Then Execute query
