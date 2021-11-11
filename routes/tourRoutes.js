cf5db51568a8c0461fdef8011cdf2d5aced1b125
Timothy Dykes <timothywanedykes@gmail.com>
2021-11-10 19:24:10 -0800

    Add to aggregation pipeline functionality

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,7 +10,6 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -33,7 +33,7 @@ class APIFeatures {
   }
 
   limitFields() {
-    if (this.queryStr) {
+    if (this.queryStr.fields) {
       const fields = this.queryStr.fields.split(',').join(' ');
       this.query = this.query.select(fields);
     } else {
@@ -44,7 +44,7 @@ class APIFeatures {
 
   paginate() {
     const page = this.queryStr.page * 1 || 1;
-    const limit = this.queryStr.limit * 1 || 1;
+    const limit = this.queryStr.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
