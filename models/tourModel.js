c183b83575d9575e045a5cf5bab3b131548e82f3
Timothy Dykes <timothywanedykes@gmail.com>
2021-11-16 22:04:34 -0800

    Add built-in validators for data

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less than 40 characters'],
+      minlength: [10, 'A tour name must have more than 10 characters'],
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A group must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'diffficult'],
+        message: 'Difficulty can only be easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be greater than 0'],
+      max: [5, 'Rating must be less than 5'],
     },
     ratingsQuantity: {
       type: Number,
