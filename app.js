5752d934823e1c48e6757e65d30b0a10cee16312
Timothy Dykes <timothywanedykes@gmail.com>
2021-11-29 17:18:38 -0800

    Add handler for unhandled routes

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // add my own middleware function
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -30,5 +26,13 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handler for unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl}`,
+  });
+});
 
 module.exports = app;
