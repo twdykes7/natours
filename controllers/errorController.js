1d0580bdb74d0079b3e09beea49f5728bc5ab835
Timothy Dykes <timothywanedykes@gmail.com>
2021-12-01 15:02:33 -0800

    Add seperate errors for production and development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,42 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or unknown error: don't leak error to client
+  } else {
+    // 1 log error
+    console.error('Error', err);
+
+    // 2 Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'err';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 
   res.status(err.statusCode).json({
     status: err.status,
